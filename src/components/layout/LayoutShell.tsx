7627f5ab5a0b45d58c2c155b4e5706a75d89c734
jivraj kashid <164367896+jivrajx04@users.noreply.github.com>
2025-11-21 14:58:29 +0530

    Updated LayoutShell.tsx

--- src/components/layout/LayoutShell.tsx
+++ src/components/layout/LayoutShell.tsx
@@ -8,14 +8,14 @@ interface LayoutShellProps {
 
 export default function LayoutShell({ children }: LayoutShellProps) {
   return (
-    <div className="relative min-h-screen bg-slate-50">
+    <>
       <Sidebar />
       <Navbar />
-      <main className="ml-[260px] mt-16 h-[calc(100vh-64px)] overflow-y-auto">
+      <main className="ml-[260px] mt-16 h-[calc(100vh-64px)] overflow-y-auto bg-slate-50">
         <div className="p-8">
           {children}
         </div>
       </main>
-    </div>
+    </>
   );
 }
